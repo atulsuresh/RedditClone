6f0c9afaa5b424fd5fd037566625e9392ec31570
Athul Suresh <atulsuresh123@gmail.com>
2014-12-01 13:35:39 +0530

    removed options frm wiredep

--- Gruntfile.js
+++ Gruntfile.js
@@ -162,9 +162,7 @@ module.exports = function (grunt) {
 
     // Automatically inject Bower components into the app
     wiredep: {
-      options: {
-        //cwd: '<%= yeoman.app %>'
-      },
+
       app: {
         src: ['<%= yeoman.app %>/index.html'],
         ignorePath:  /..\//
